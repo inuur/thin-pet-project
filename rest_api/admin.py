63e84ad59cd8dfe9860f445560811975fe3acdb9
Ainur Gimadeev <inuur@mail.ru>
2022-05-14 19:03:53 +0300

    Add models to admin site

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -5,3 +5,5 @@ from .models import *
 # Register your models here.
 admin.site.register(TectonicConfinement)
 admin.site.register(MineralDeposit)
+admin.site.register(Well)
+admin.site.register(Sample)
