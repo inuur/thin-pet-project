194318769361808da05a09752f03864af3c93604
Ainur Gimadeev <inuur@mail.ru>
2022-05-14 16:22:35 +0300

    Refactor cide

--- rest_api/models.py
+++ rest_api/models.py
@@ -2,18 +2,14 @@ from django.db import models
 
 
 class TectonicConfinement(models.Model):
-    name = models.CharField(
-        max_length=255
-    )
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 
 class MineralDeposit(models.Model):
-    name = models.CharField(
-        max_length=255
-    )
+    name = models.CharField(max_length=255)
     tectonic_confinement = models.ForeignKey(
         TectonicConfinement,
         related_name='mineral_deposits',
